b1ada522d176b4cb2ccd4b9ed7379dab11a93171
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-09-22 15:41:13 +0200

    setup for multi threaded server

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
+mod single_thread_server;
+
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn main() {
-    // 127.0.0.1:7878 is localhost -> check in browser
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    // Iterating over the connections...
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
@@ -17,22 +17,9 @@ fn main() {
     }
 
     fn handle_connection(mut stream: TcpStream) {
-        // 1024 bytes long (allows to store basic requests) -> in production server we would need to make arbitrary buffer
         let mut buffer = [0; 1024];
         stream.read(&mut buffer).unwrap();
 
-        // 'from_utf8_lossy' -> converts a slice of bytes to a string including invalid characters
-        // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-
-        // Handling 'Response'
-        // Response structure:
-        // HTTP-Version Status-Code Reason-Phrase CRLS
-        // Headers CRLF
-        // message-body
-        //
-        // ex: HTTP/1.1 200 OK\r\n\r\n
-
-        // Adding 'b' at start turning string into bytes
         let get = b"GET / HTTP/1.1\r\n";
 
         let (status_line, filename) = if buffer.starts_with(get) {
