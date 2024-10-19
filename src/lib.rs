696078a16dadaaf355309f912944a54aec905739
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-10-19 17:02:33 +0200

    drop method implemented to handle server shut down once jobs done

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub mod single_thread_server;
 pub struct ThreadPool {
     #[allow(dead_code)]
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -43,12 +43,20 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending terminate message to all workers...");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers...");
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
@@ -65,12 +73,20 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv().unwrap();
 
-            println!("Worker {} got a job: executing...", id);
-            job();
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job: executing...", id);
+                    job()
+                }
+                Message::Terminate => {
+                    println!("Worker {} was called to terminate...", id);
+                    break;
+                }
+            }
         });
 
         Worker {
